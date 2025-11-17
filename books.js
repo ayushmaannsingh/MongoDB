668ea8d3a33055939b0cdd3eb588edbd1e8d0ea3
ayushmaannsingh <sa776857@gmail.com>
2025-11-17 12:33:52 +0530

    added books.js

---
+++ books.js
@@ -0,0 +1,29 @@
+// getting-started.js
+const mongoose = require('mongoose');
+
+main()
+  .then(() => {
+    console.log("connection successful");
+    
+  })
+  .catch((err) => console.log(err)
+  );
+
+async function main() {
+  await mongoose.connect('mongodb://127.0.0.1:27017/amazon');
+
+  // use `await mongoose.connect('mongodb://user:password@127.0.0.1:27017/test');` if your database has auth enabled
+}
+
+const bookSchema = new mongoose.Schema({
+    title: {
+        type: String,
+        required: true,
+    },
+    author: {
+        type: String,
+    },
+    price: {
+        type: Number,
+    },
+});
